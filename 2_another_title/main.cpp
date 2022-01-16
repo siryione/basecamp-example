ba6ede17ae11778568910a078014465fc3f25936
Ivan Siryi <siryione@gmail.com>
2022-01-16 16:22:25 +0200

    Add task 2 implementation

---
+++ 2_another_title/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+
+int main(){
+    // DECLARING POINTERS
+    // declare and assign 
+
+    int *ptr_int_to_var;
+    char *ptr_char_to_const;
+    const float *ptr_float_const_to_var;
+    const bool *ptr_bool_const_to_const;
+
+    int var_int = 1;
+    float var_float = 0.2f;
+    const char var_const_char = 'A';
+    const bool var_const_bool = true;
+
+    ptr_int_to_var = &var_int;
+    // ptr_char_to_const = &var_const_char; -- error: invalid conversion from ‘const char*’ to ‘char*’ [-fpermissive]
+    ptr_float_const_to_var = &var_float;
+    ptr_bool_const_to_const = &var_const_bool;
+
+    // change pointer's value
+
+    *ptr_int_to_var = 10;
+    // *ptr_char_to_const = 'B'; -- Segmentation fault
+    // *ptr_float_const_to_var = 0.1f; -- error: assignment of read-only location ‘* ptr_float_const_to_var’
+    // *ptr_bool_const_to_const = false; -- error: assignment of read-only location ‘* ptr_bool_const_to_const’
+
+    // change var's value
+
+    var_int = 100;
+    var_float = 100.f;
+    // var_const_char = 'a'; -- error: assignment of read-only variable ‘var_const_char’
+    // var_const_bool = true; -- error: assignment of read-only variable ‘var_const_bool’
+    
+
+
+    // ARRAYS AND POINTER ARITHMETIC
+    // create a C-style array of some type and fill it with values
+
+    char arr[] = {'A', 'a', 'b', 'B'};
+    // char arr[] = {'A', 'a', 'b', 'B', '\00'};
+    // char arr[] = "AaBb";
+    
+    // modify and print each value of the array
+
+    char mod_values[] = {'c', 'd', 'e', 'f'};
+    for(int index = 0; index < sizeof(arr) && index < sizeof(mod_values); index++){
+        std::cout << index << "# arr index value before modification: " << arr[index] << std::endl;
+        arr[index] = {mod_values[index]};
+        std::cout << index << "# arr index value after modification: " << arr[index] << std::endl;
+    }
+
+    // declare a pointer for the beginning of the arr
+
+    char *ptr_arr = arr;
+    std::cout << "[log]: arr address start: " << &ptr_arr << std::endl;
+
+    // modify and print values using pointer arithmetic
+
+    char mod_ptr_values[] = {'C', 'D', 'E', 'F'};
+    for(int index = 0; index < sizeof(arr) && index < sizeof(mod_ptr_values); index++){
+        std::cout << index << "# mem cell value before modification: " << *ptr_arr << std::endl;
+        *ptr_arr = {mod_ptr_values[index]};
+        std::cout << index << "# mem cell value after modification: " << *ptr_arr << std::endl;
+
+        ptr_arr++;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
