ee4b87c5d43d9868e06faa8dea02c5152f504ad7
Ivan Siryi <siryione@gmail.com>
2022-01-20 13:38:27 +0200

    Add extra task implementation

--- 2_another_title/main.cpp
+++ 2_another_title/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#define COLUMN 4
+#define ROW 2
 
 
 int main(){
@@ -55,14 +57,16 @@ int main(){
     // declare a pointer for the beginning of the arr
 
     char *ptr_arr = arr;
-    std::cout << "[log]: arr address start: " << &ptr_arr << std::endl;
+    std::cout << "\nArray address: " << &ptr_arr << std::endl;
 
     // modify and print values using pointer arithmetic
 
+
     char mod_ptr_values[] = {'C', 'D', 'E', 'F'};
     for(int index = 0; index < sizeof(arr) && index < sizeof(mod_ptr_values); index++){
-        std::cout << index << "# mem cell value before modification: " << *ptr_arr << std::endl;
-        *ptr_arr = {mod_ptr_values[index]};
+        
+        std::cout << index << "# mem cell value before modification: " << *ptr_arr << ", int adress: " << (long int)ptr_arr << std::endl;
+        *ptr_arr = mod_ptr_values[index];
         std::cout << index << "# mem cell value after modification: " << *ptr_arr << std::endl;
 
         ptr_arr++;
@@ -71,13 +75,46 @@ int main(){
 
 
 
+    // EXTRA TASK 
+    std::cout << "\nExtra task" << std::endl;
+    char arr_2dim[ROW][COLUMN] = {{'A', 'a', 'b', 'B'}, {'c', 'd', 'e', 'f'}};
 
+    
+    // modify and print each value of the array
 
+    char mod_values_2dim[ROW][COLUMN] = {{'c', 'd', 'e', 'f'}, {'A', 'a', 'b', 'B'}};
+    int index = 0;
+    for(int row = 0; row < ROW; row++){
+        for(int column = 0; column < COLUMN; column++){
 
+        std::cout << index << "# arr element value before modification: " << arr_2dim[row][column] << std::endl;
+        arr_2dim[row][column] = {mod_values_2dim[row][column]};
+        std::cout << index << "# arr element value after modification: " << arr_2dim[row][column] << std::endl;
+        }
+    }
+    index = 0;
 
+    // declare a pointer for the beginning of the arr
 
+    char (*ptr_arr_2dim)[COLUMN] = arr_2dim;
+    std::cout << "\nArray address: " << &ptr_arr_2dim << std::endl;
 
+    // modify and print values using pointer arithmetic
 
+    char mod_ptr_values_2dim[ROW][COLUMN] = {{'C', 'D', 'E', 'F'}, {'G', 'H', 'I', 'J'}};
+    for(int row = 0; row < ROW; row++){
+        for(int column = 0; column < COLUMN; column++){
+
+            std::cout << index << "# mem cell value before modification: " << (*ptr_arr_2dim)[column] << ", int adress: " << index + (long int)ptr_arr_2dim << std::endl;
+            (*ptr_arr_2dim)[column] = mod_ptr_values_2dim[row][column];
+
+            std::cout << index << "# mem cell value after modification: " << (*ptr_arr_2dim)[column] << std::endl;
+            
+            index++;
+        }
+        ptr_arr_2dim++;
+    }
+    index = 0;
 
     return 0;
 }
